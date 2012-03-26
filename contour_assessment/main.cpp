d69ed1cd03cfb78ca7a9054371ddd55bd33e0c69
Di YANG <diyang@Di-YANGs-MacBook.local>
2012-03-26 14:59:36 +1100

    Initial Commit

---
+++ contour_assessment/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  contour_assessment
+//
+//  Created by Di Yang on 26/03/12.
+//  Copyright (c) 2012 The Australian National University. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
